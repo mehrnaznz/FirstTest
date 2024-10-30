570d98c788d1fdda174b2697d3ff36f1f6fcda68
mehrnaz <mehrnaz.niyazi@gmail.com>
2024-10-30 22:53:12 +0100

    add new piece of code

--- Mehr.py
+++ Mehr.py
@@ -1,4 +1,9 @@
 
-d={"a":1, "b":2, "c":3}
-for i in d:
-    print(i+"->"+str(d[i]))
+dictionary={"a":1, "b":2, "c":3}
+for key,value in dictionary.items():
+    print(str(key) + "±±±±" + str(value))
+
+
+listtttttt = ["ali", "mehr", "benhamz", 1, 4 ,5.87, "Lily"]
+for i in listtttttt:
+    print(i)
